f56b1b6bff17b2f71ff480d31d9a687bb1c67420
hs13 <hs13@users.noreply.github.com>
2024-09-13 18:41:47 +0900

    edited postprocess_lfp

--- postprocess_lfp.m
+++ postprocess_lfp.m
@@ -1,4 +1,5 @@
 addpath(genpath('/Users/hyeyoung/Documents/CODE/matnwb'))
+addpath(genpath('d:\Users\USER\Documents\MATLAB\matnwb'))
 
 datadir = 'S:\OpenScopeData\00248_v240130\';
 nwbdir = dir(datadir);
@@ -7,18 +8,30 @@ nwbsessions = nwbsessions( contains(nwbsessions, 'sub-') | contains(nwbsessions,
 
 probes = {'A', 'B', 'C', 'D', 'E'};
 
+pathpp = [datadir 'postprocessed' filesep nwbsessions{ises} filesep];
+load('')
 nwbfiles = cat(1, dir([datadir nwbsessions{ises} filesep '*.nwb']), dir([datadir nwbsessions{ises} filesep '*' filesep '*.nwb']));
 nwblfpfiles = nwbfiles(contains({nwbfiles.name}, 'probe'));
 
-% for iprobe = 0:5
-fileind = find(contains({nwbfiles.name}, ['probe-' num2str(iprobe)]));
+%% load lfp data for each probe
+% for probeind = 0:5
+fileind = find(contains({nwbfiles.name}, ['probe-' num2str(probeind)]));
 nwblfpfile = fullfile([nwbfiles(fileind).folder filesep nwbfiles(fileind).name]);
 
 nwblfp = nwbRead(nwblfpfile); 
 
-%lfp_probe = nwblfp.acquisition.get('probe_2_lfp');
-lfp = nwblfp.acquisition.get('probe_2_lfp_data');
-lfpdata = lfp.data.load();
-lfelectrodes = lfp.data.load();
+% lfp = nwblfp.acquisition.get('probe_2_lfp_data');
+% lfpdata = lfp.data.load();
 
-nwblfp.processing
+lfp_probe = nwblfp.acquisition.get('probe_2_lfp');
+lfp = lfp_probe.electricalseries.get('probe_2_lfp_data');
+lfpdata = lfp.data.load(); % nprobes * ntimepoints
+lfptimestaps = lfp.timestamps.load(); % ntimepoints * 1, units in seconds
+
+
+%% figure out which electrode is where
+
+
+%% align to vis stim
+
+%% align to opto stim
