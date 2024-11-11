b6ab6d495bda696055b2a067bd98282a8a2fd373
hs13 <hs13@users.noreply.github.com>
2024-11-11 12:44:45 +0900

    241111

--- computeBayesianInferenceDecoding.m
+++ computeBayesianInferenceDecoding.m
@@ -1,5 +1,6 @@
 if ismac
     drivepath = '/Users/hyeyoung/Library/CloudStorage/GoogleDrive-shinehyeyoung@gmail.com/My Drive/';
+    codepath = '/Users/hyeyoung/Documents/CODE/Analyze_IC_OpenScope_v240130/';
 else
     drivepath = 'G:/My Drive/';
     codepath = 'C:\Users\USER\GitHub\';
@@ -8,43 +9,474 @@ end
 addpath([codepath 'helperfunctions'])
 load([drivepath 'RESEARCH/logmean_logvar/OpenScope_spkcnt_ICwcfg1.mat'])
 
+twin = 0.4; % in sec
+testt = [106 107 110 111];
+Ntt = numel(testt);
+inferencett = [1105 1109];
 
 ises = 3;
-nwbsessions{ises}
+spkcntses = spkcntIChiV1agg{ises};
 
 %% comparison point: linear SVM
 load(['S:\OpenScopeData\00248_v240130\SVM_trainICRC_selectareas\' nwbsessions{ises} '\SVM_trainICRC_VISpRS_Linear_zscore_ICwcfg1.mat'])
-Ntt = numel(SVMtrainICRC.trialtypes);
 
-testtrialinds = SVMtrainICRC.spkcnt.testtrialinds;
+if ~isequal(SVMtrainICRC.trialtypes, testt)
+    error('mismatch in test trial types: check that you loaded trainICRC')
+end
 
 % test accuracy
 testlabs = SVMtrainICRC.trialorder(SVMtrainICRC.spkcnt.testtrialinds);
 testpred = SVMtrainICRC.spkcnt.test.label;
 testacc = zeros(Ntt);
 for itt = 1:Ntt
-    trialsoi = testlabs==SVMtrainICRC.trialtypes(itt);
+    trialsoi = testlabs==testt(itt);
     [v,c]=uniquecnt(testpred(trialsoi));
-    testacc(itt, ismember(SVMtrainICRC.trialtypes,v)) = c/nnz(trialsoi);
+    testacc(itt, ismember(testt,v)) = c/nnz(trialsoi);
 end
 
 % inference decoding
-inferencett = [1105 1109];
 inftrials = ismember(SVMtrainICRC.trialorder, inferencett);
 infpred = SVMtrainICRC.spkcnt.all.label(inftrials,:);
 infperf = zeros(numel(inferencett), Ntt);
 for itt = 1:numel(inferencett)
     trialsoi = SVMtrainICRC.trialorder(inftrials)==inferencett(itt);
     [v,c]=uniquecnt(infpred(trialsoi));
-    infperf(itt, ismember(SVMtrainICRC.trialtypes,v)) = c/nnz(trialsoi);
+    infperf(itt, ismember(testt,v)) = c/nnz(trialsoi);
 end
 
 %% Bayesian image decoding (inspired by position decoding in hippocampus literature, e.g., Buzsaki lab, Fenton lab)
 
+filtervalneu = false; % whether to filter out neurons that hardly ever fire
+% NO NEED TO WORRY about this because 0^0=1
+
+% train vs test trial divide
+if exist('SVMtrainICRC', 'var')
+    kfold = size(SVMtrainICRC.spkcnt.testtrialinds,2);
+    traintrialinds = cell(size(hireptt));
+    testtrials = cell(size(hireptt));
+    for itt = 1:Ntt
+        trialsoind = find(SVMtrainICRC.trialorder==inferencett(itt));
+        typi = hireptt==testt(itt);
+        ntrials = size(spkcntses{typi},1);
+        testtrials{typi} = false(ntrials,kfold);
+        for k = 1:kfold
+            testtrials{typi}(:,k) = ismember(trialsoind, SVMtrainICRC.spkcnt.testtrialinds(:,k));
+        end
+    end
+else
+    kfold = 10;
+    testtrials = cell(size(hireptt));
+    for itt = 1:Ntt
+        typi = hireptt==testt(itt);
+        ntrials = size(spkcntses{typi},1);
+        testtrials{typi} = false(ntrials,kfold);
+        c = cvpartition(ntrials,"KFold",kfold);
+        for k = 1:kfold
+            testtrials{typi}(:,k) = test(c,k);
+        end
+    end
+end
+
+Nneurons = size(spkcntses{1},2);
+
+% filter out neurons whose FRtrainavg could be zero
+neuval = true(Nneurons,1);
+if filtervalneu
+    for itt = 1:Ntt
+        typi = hireptt==testt(itt);
+        tempvalneu = mean(spkcntses{typi}>0,1) >= 1/kfold;
+        neuval(~tempvalneu) = false;
+    end
+end
+
+bayesimage = struct();
+bayesimage.postprob = cell(numel(hireptt),kfold);
+bayesimage.postprobnorm = cell(numel(hireptt),kfold);
+bayesimage.trainacc = zeros(Ntt,Ntt,kfold);
+bayesimage.testacc = zeros(Ntt,Ntt,kfold);
+bayesimage.infperf = zeros(numel(inferencett),Ntt,kfold);
+for k = 1:kfold
+    
+    FRtrainavg = NaN(nnz(neuval), Ntt);
+    for itt = 1:Ntt
+        typi = hireptt==testt(itt);
+        FRtrainavg(:,itt) = (1/twin) * mean(spkcntses{typi}(~testtrials{typi}(:,k), neuval),1);
+    end
+    
+    for typi = 1:numel(hireptt)
+        ntrials = size(spkcntses{typi},1);
+        bayesimage.postprob{typi,k} = NaN( ntrials, Ntt ); % intialize
+        for itrial = 1:ntrials
+            spkcntvec = spkcntses{typi}(itrial,neuval)';
+            for jtt = 1:Ntt
+                FRimvec = FRtrainavg(:,jtt);
+                bayesimage.postprob{typi,k}(itrial,jtt) = prod(FRimvec.^spkcntvec) * exp(-twin*sum(FRimvec));
+            end
+        end
+        bayesimage.postprobnorm{typi,k} = bayesimage.postprob{typi,k}./sum(bayesimage.postprob{typi,k},2);
+    end
+        
+    for itt = 1:Ntt
+        typi = hireptt==testt(itt);
+        temptesttrials = testtrials{typi}(:,k);
+        [mv,mi] = max( bayesimage.postprobnorm{typi,k}(~temptesttrials,:),[],2);
+        [v,c]=uniquecnt(mi);
+        bayesimage.trainacc(itt, ismember([1,2,3,4], v), k) = c/nnz(~temptesttrials);
+        
+        [mv,mi] = max( bayesimage.postprobnorm{typi,k}(temptesttrials,:),[],2);
+        [v,c]=uniquecnt(mi);
+        bayesimage.testacc(itt, ismember([1,2,3,4], v), k) = c/nnz(temptesttrials);
+    end    
+    
+    for itt = 1:numel(inferencett)
+        typi = hireptt==inferencett(itt);
+        ntrials = size(bayesimage.postprobnorm{typi,k},1);
+        [mv,mi] = max( bayesimage.postprobnorm{typi,k},[],2);
+        [v,c]=uniquecnt(mi);
+        bayesimage.infperf(itt, ismember([1,2,3,4], v), k) = c/ntrials;
+    end
+end
+
+disp('bayesimage.trainacc')
+disp(mean(bayesimage.trainacc,3))
+disp('bayesimage.testacc')
+disp(mean(bayesimage.testacc,3))
+disp('bayesimage.infperf')
+disp(mean(bayesimage.infperf,3))
+
+%% Naive Bayes Gaussian Decoder: fit gaussian to log spike count
+% contruct P(r|s) assuming that spike counts are log normal (train vs test split)
+% sum P(r|s) across neurons
+% normalize across stimuli
+
+% adding spike count offset of 1 *increases* train, test and inference
+% performance!!!
+spkcntoffset = 1;
+
+%{
+% check gaussian fit of log(spike counts)
+typi = hireptt==111;
+[sv,si] = sort(mean(spkcntses{typi},1), 'descend');
+neuex = si(100);
+
+figure
+for offset = 0:1
+
+    tempspk = offset+spkcntses{typi}(:,neuex);
+
+xval = log10(tempspk(tempspk>0));
+[muHat,sigmaHat] = normfit(xval);
+mu = mean(xval);
+sigma = std(xval);
+subplot(2,2,1+offset*2); hold all
+h = histogram(xval, 'normalization', 'pdf');
+xt = h.BinEdges(1):0.001:h.BinEdges(end);
+y = normpdf(xt,mu,sigma);
+yHat = normpdf(xt,muHat,sigmaHat);
+y1 = normpdf(xt,mean(log10(tempspk+10^-1)), std(log10(tempspk+10^-1)) );
+y2 = normpdf(xt,mean(log10(tempspk+10^0)), std(log10(tempspk+10^0)) );
+plot(xt, y, 'k-', 'linewidth',2)
+plot(xt, yHat, 'r--', 'linewidth',1.5)
+plot(xt, y1, 'b--', 'linewidth',2)
+plot(xt, y2, 'c--', 'linewidth',2)
+
+subplot(2,2,2+offset*2); hold all
+h = histogram(tempspk, 'normalization', 'pdf');
+xt = log10(h.BinEdges(2:end));
+y = normpdf(xt,mu,sigma);
+yHat = normpdf(xt,muHat,sigmaHat);
+y1 = normpdf(xt,mean(log10(tempspk+10^-1)), std(log10(tempspk+10^-1)) );
+y2 = normpdf(xt,mean(log10(tempspk+10^0 )), std(log10(tempspk+10^0)) );
+plot(10.^xt, 10.^y*sum(h.Values)/sum(10.^y), 'k-', 'linewidth',2)
+plot(10.^xt, 10.^yHat*sum(h.Values)/sum(10.^yHat), 'r--', 'linewidth',2)
+plot(10.^xt, 10.^y1*sum(h.Values)/sum(10.^y1), 'b--', 'linewidth',2)
+plot(10.^xt, 10.^y2*sum(h.Values)/sum(10.^y2), 'c--', 'linewidth',2)
+end
+%}
+
+naivegauss = struct();
+naivegauss.postprob = cell(numel(hireptt),kfold);
+naivegauss.postprobnorm = cell(numel(hireptt),kfold);
+naivegauss.trainacc = zeros(Ntt,Ntt,kfold);
+naivegauss.testacc = zeros(Ntt,Ntt,kfold);
+naivegauss.infperf = zeros(numel(inferencett),Ntt,kfold);
+for k = 1:kfold
+    for typi = 1:numel(hireptt)
+        ntrials = size(spkcntses{typi},1);
+        ratelikelihood = zeros(Nneurons, ntrials, Ntt);
+        for jtt = 1:Ntt
+            typj = hireptt==testt(jtt);
+            temptesttrials = testtrials{typj}(:,k);
+            for ci = 1:Nneurons
+                tempspk = spkcntoffset+spkcntses{typj}(~temptesttrials, ci);
+                xval = log10(tempspk(tempspk>0));
+                mu = mean(xval);
+                sigma = std(xval);
+                xtt = log10(spkcntoffset+spkcntses{typi}(:,ci));
+                ratelikelihood(ci,:,jtt) = normpdf(xtt,mu,sigma);
+            end
+        end
+        naivegauss.postprob{typi,k} = squeeze(nansum(ratelikelihood,1));
+        
+%         % sum across neurons then normalize across trialtypes
+%         normsumratelikelihood = squeeze( nansum(ratelikelihood,1)./sum(nansum(ratelikelihood,1),3) );
+%         naivegauss.postprobnorm{typi,k} = normsumratelikelihood;
+        
+        % normalize each neuron, then sum across neurons
+        % this leads to higher train and test accuracy
+        sumnormratelikelihood = squeeze(nansum( ratelikelihood./sum(ratelikelihood,3), 1));
+        naivegauss.postprobnorm{typi,k} = sumnormratelikelihood;
+    end
+
+    for itt = 1:Ntt
+        typi = hireptt==testt(itt);
+        temptesttrials = testtrials{typi}(:,k);
+        [mv,mi] = max( naivegauss.postprobnorm{typi,k}(~temptesttrials,:),[],2);
+        [v,c]=uniquecnt(mi);
+        naivegauss.trainacc(itt, ismember([1,2,3,4], v), k) = c/nnz(~temptesttrials);
+        
+        [mv,mi] = max( naivegauss.postprobnorm{typi,k}(temptesttrials,:),[],2);
+        [v,c]=uniquecnt(mi);
+        naivegauss.testacc(itt, ismember([1,2,3,4], v), k) = c/nnz(temptesttrials);
+    end    
+    
+    for itt = 1:numel(inferencett)
+        typi = hireptt==inferencett(itt);
+        ntrials = size(naivegauss.postprobnorm{typi,k},1);
+        [mv,mi] = max( naivegauss.postprobnorm{typi,k},[],2);
+        [v,c]=uniquecnt(mi);
+        naivegauss.infperf(itt, ismember([1,2,3,4], v), k) = c/ntrials;
+    end
+end
+
+disp('naivegauss.trainacc')
+disp(mean(naivegauss.trainacc,3))
+disp('naivegauss.testacc')
+disp(mean(naivegauss.testacc,3))
+disp('naivegauss.infperf')
+disp(mean(naivegauss.infperf,3))
+
+%% Bayesian multivariate gaussian decoder
+% for N neurons, fit N-dimensional multivarite gaussian
+% unlike Naive Bayes, covariance is factored in
+% however, fitting multivariate gaussian performs much more poorly than naive bayes, probably due to poor fit
+
+filtervalneu = false; % whether to filter out neurons that hardly ever fire
+% filter out neurons whose FRtrainavg could be zero
+neuval = true(Nneurons,1);
+if filtervalneu
+    for itt = 1:Ntt
+        typi = hireptt==testt(itt);
+        tempvalneu = mean(spkcntses{typi}>0,1) >= 1/kfold;
+        neuval(~tempvalneu) = false;
+    end
+end
+
+%{
+% check multivariate gaussian fit of log(spike counts) for a pair of neurons
+typi = hireptt==106;
+[sv,si] = sort(mean(spkcntses{typi},1), 'descend');
+neuexi = si(1);
+neuexj = si(2);
+
+tempspki = spkcntoffset+spkcntses{typi}(:,neuexi);
+tempspkj = spkcntoffset+spkcntses{typi}(:,neuexj);
+
+tempspkn0 = [tempspki tempspkj];
+tempspkn0(tempspkn0==0) = NaN;
+X = log10(tempspkn0);
+mu = nanmean(X,1);
+Sigma = cov(X, 'partialrows');
+
+% xxt = log10(1):0.001:log10(max(tempspki));
+% yyt = log10(1):0.001:log10(max(tempspkj));
+xxt = log10(1:1:max(tempspki));
+yyt = log10(1:1:max(tempspkj));
+gridxyticks = zeros(length(yyt), length(xxt), 2);
+gridxyticks(:,:,1) = repmat(xxt, length(yyt), 1);
+gridxyticks(:,:,2) = repmat(yyt', 1, length(xxt) );
+gridxy = reshape(gridxyticks, length(yyt)*length(xxt), 2);
+gridpdf = mvnpdf(gridxy,mu,Sigma);
+gridpdf = reshape(gridpdf, length(yyt), length(xxt));
+
+redcm = 1-gray;
+redcm(:,1)=1;
+figure
+hold all
+imagesc(10.^xxt, 10.^yyt, gridpdf);%, 'AlphaData',0.1)
+scatter(tempspki, tempspkj, 'o', 'MarkerFaceAlpha', 0.2, 'MarkerEdgeColor', 'none', 'MarkerFaceColor', 'k')
+colormap(redcm)
+%}
+
+mvnbayes = struct();
+mvnbayes.postprob = cell(numel(hireptt),kfold);
+mvnbayes.postprobnorm = cell(numel(hireptt),kfold);
+mvnbayes.trainacc = zeros(Ntt,Ntt,kfold);
+mvnbayes.testacc = zeros(Ntt,Ntt,kfold);
+mvnbayes.infperf = zeros(numel(inferencett),Ntt,kfold);
+for k = 1:kfold
+    for typi = 1:numel(hireptt)
+        ntrials = size(spkcntses{typi},1);
+        mvnbayes.postprob{typi,k} = NaN( ntrials, Ntt );
+        for jtt = 1:Ntt
+            typj = hireptt==testt(jtt);
+            temptesttrials = testtrials{typj}(:,k);
+            
+            tempspkn0 = spkcntoffset+spkcntses{typi}(~temptesttrials,neuval);
+            tempspkn0(tempspkn0==0) = NaN;
+            X = log10(tempspkn0);
+            mu = nanmean(X,1);
+            Sigma = cov(X, 'partialrows');
+            xtt = log10( spkcntoffset+spkcntses{typi}(:,neuval) );
+            try
+                mvnbayes.postprob{typi,k}(:,jtt) = mvnpdf(xtt,mu,Sigma);
+            catch
+                % SIGMA must be a square, symmetric, positive definite matrix.
+                % Positive definite matrix: all eigenvalues are positive;
+                % the matrix is guaranteed to be invertible.
+                % Step 1: Symmetrize A
+                Sigma = (Sigma + Sigma') / 2;
+                % Step 2: Adjust eigenvalues to ensure positive definiteness
+                [eigVec, eigVal] = eig(Sigma);
+                eigVal(eye(size(eigVal)) & eigVal<=1e-20) = 1e-20; % Shift non-positive eigenvalues
+                Sigma = eigVec * eigVal * eigVec';
+                mv = max(abs(cov(X, 'partialrows')-eigVec * eigVal * eigVec'),[],'all');
+                if mv>10^-10
+                warning('difference after making Sigma square, symmetric, positive definite')
+                end
+                %figure; plot(cov(X, 'partialrows'), eigVec * eigVal * eigVec', '.')
+                
+                mvnbayes.postprob{typi,k}(:,jtt) = mvnpdf(xtt,mu,Sigma);
+            end
+            
+        end
+        mvnbayes.postprobnorm{typi,k} = mvnbayes.postprob{typi,k}./sum(mvnbayes.postprob{typi,k},2);
+    end
+    
+    for itt = 1:Ntt
+        typi = hireptt==testt(itt);
+        temptesttrials = testtrials{typi}(:,k);
+        [mv,mi] = max( mvnbayes.postprobnorm{typi,k}(~temptesttrials,:),[],2);
+        [v,c]=uniquecnt(mi);
+        mvnbayes.trainacc(itt, ismember([1,2,3,4], v), k) = c/nnz(~temptesttrials);
+        
+        [mv,mi] = max( mvnbayes.postprobnorm{typi,k}(temptesttrials,:),[],2);
+        [v,c]=uniquecnt(mi);
+        mvnbayes.testacc(itt, ismember([1,2,3,4], v), k) = c/nnz(temptesttrials);
+    end
+    
+    for itt = 1:numel(inferencett)
+        typi = hireptt==inferencett(itt);
+        ntrials = size(mvnbayes.postprobnorm{typi,k},1);
+        [mv,mi] = max( mvnbayes.postprobnorm{typi,k},[],2);
+        [v,c]=uniquecnt(mi);
+        mvnbayes.infperf(itt, ismember([1,2,3,4], v), k) = c/ntrials;
+    end
+end
+
+disp('mvnbayes.trainacc')
+disp(mean(mvnbayes.trainacc,3))
+disp('mvnbayes.testacc')
+disp(mean(mvnbayes.testacc,3))
+disp('mvnbayes.infperf')
+disp(mean(mvnbayes.infperf,3))
+
+%% Naive Bayes Gaussian PCA Decoder: fit gaussian to PCA
+% independence assumtion becomes valid after PCA
+logspkopt = false; % logspkopt==true returns a much poorer performance!!!
+
+pcanaivegauss = struct();
+pcanaivegauss.postprob = cell(numel(hireptt),kfold);
+pcanaivegauss.postprobnorm = cell(numel(hireptt),kfold);
+pcanaivegauss.trainacc = zeros(Ntt,Ntt,kfold);
+pcanaivegauss.testacc = zeros(Ntt,Ntt,kfold);
+pcanaivegauss.infperf = zeros(numel(inferencett),Ntt,kfold);
+for k = 1:kfold
+    for typi = 1:numel(hireptt)
+        ntrials = size(spkcntses{typi},1);
+        
+
+        ratelikelihood = zeros(Nneurons, ntrials, Ntt);
+        for jtt = 1:Ntt
+            typj = hireptt==testt(jtt);
+            temptesttrials = testtrials{typj}(:,k);
+            if logspkopt
+                tempspk = spkcntoffset+spkcntses{typj}(~temptesttrials, :);
+                Xval = log10(tempspk(tempspk>0));
+                Xtt = log10(spkcntoffset+spkcntses{typi});
+            else
+                Xval = spkcntses{typj}(~temptesttrials, :);
+                Xtt = spkcntses{typi};
+            end
+            [COEFF, SCORE, LATENT, TSQUARED, EXPLAINED, MU] = pca(Xval);
+            mcspkcnt = Xtt-MU;
+            SCOREtt = mcspkcnt*COEFF;
+            for ci = 1:size(COEFF,2)
+                mu = mean(SCORE(:,ci));
+                sigma = std(SCORE(:,ci));
+                ratelikelihood(ci,:,jtt) = normpdf(SCOREtt(:,ci),mu,sigma);
+                
+                %{
+                % the following takes much longer and does not improve performance
+                try
+                    GMModel = fitgmdist(SCORE(:,ci),3);
+                    ratelikelihood(ci,:,jtt) = pdf(GMModel, SCOREtt(:,ci));
+                catch
+                    GMModel = fitgmdist(SCORE(:,ci),1);
+                    ratelikelihood(ci,:,jtt) = pdf(GMModel, SCOREtt(:,ci));
+                end
+                %}
+            end
+        end
+        pcanaivegauss.postprob{typi,k} = squeeze(nansum(ratelikelihood,1));
+        
+%         % sum across neurons then normalize across trialtypes
+%         normsumratelikelihood = squeeze( nansum(ratelikelihood,1)./sum(nansum(ratelikelihood,1),3) );
+%         pcanaivegauss.postprobnorm{typi,k} = normsumratelikelihood;
+        
+        % normalize each neuron, then sum across neurons
+        % this leads to higher train and test accuracy
+        sumnormratelikelihood = squeeze(nansum( ratelikelihood./sum(ratelikelihood,3), 1));
+        pcanaivegauss.postprobnorm{typi,k} = sumnormratelikelihood;
+    end
+
+    for itt = 1:Ntt
+        typi = hireptt==testt(itt);
+        temptesttrials = testtrials{typi}(:,k);
+        [mv,mi] = max( pcanaivegauss.postprobnorm{typi,k}(~temptesttrials,:),[],2);
+        [v,c]=uniquecnt(mi);
+        pcanaivegauss.trainacc(itt, ismember([1,2,3,4], v), k) = c/nnz(~temptesttrials);
+        
+        [mv,mi] = max( pcanaivegauss.postprobnorm{typi,k}(temptesttrials,:),[],2);
+        [v,c]=uniquecnt(mi);
+        pcanaivegauss.testacc(itt, ismember([1,2,3,4], v), k) = c/nnz(temptesttrials);
+    end    
+    
+    for itt = 1:numel(inferencett)
+        typi = hireptt==inferencett(itt);
+        ntrials = size(pcanaivegauss.postprobnorm{typi,k},1);
+        [mv,mi] = max( pcanaivegauss.postprobnorm{typi,k},[],2);
+        [v,c]=uniquecnt(mi);
+        pcanaivegauss.infperf(itt, ismember([1,2,3,4], v), k) = c/ntrials;
+    end
+end
+
+disp('pcanaivegauss.trainacc')
+disp(mean(pcanaivegauss.trainacc,3))
+disp('pcanaivegauss.testacc')
+disp(mean(pcanaivegauss.testacc,3))
+disp('pcanaivegauss.infperf')
+disp(mean(pcanaivegauss.infperf,3))
+mean(diag(mean(pcanaivegauss.testacc,3)))
+
+%% Naive Bayes PCA Decoder: 
 
 
 %% Naive Bayes Decoder (independent neurons)
+% contruct P(r|s) based on observed spike counts (train vs test split)
 
 %% AODE (Sugden...Andermann 2020)
+% acrivity was binarized based on a threshold
+
 
 %% UMAP + Bayesian decoding
---
+++ computeICtxi_SVM.m
@@ -0,0 +1,245 @@
+function [SVMout, SVM_models] = computeICtxi_SVM(tempR, trialorder, whichR, preproc, whichSVMkernel)
+
+Nsplits = 10;
+
+% whichR = 'spkcnt';
+% if size(Rall.(ICblocks{b}),2)~=length(neu2anal)
+%     error('check neu2anal')
+% end
+% tempR = Rall.(ICblocks{b})(:,neu2anal)';
+% trialorder = ICtrialtypes( vis.(ICblocks{b}).trialorder + 1);
+
+Nneurons = size(tempR,1);
+numrectrials = size(tempR,2);
+
+%% train SVM
+SVM_models = struct();
+SVMout = struct();
+
+alltrialtypes = unique(trialorder);
+switch svmdesc
+    case 'trainICRC'
+        traintrialtypes = [106, 107, 110, 111];
+        probetrialtypes = [1105, 1109];
+    case 'trainREx'
+        traintrialtypes = [1201, 1299];
+        probetrialtypes = [106, 107, 110, 111];
+    case 'trainIC1RC1'
+        traintrialtypes = [106, 107];
+        probetrialtypes = [1105];
+    case 'trainIC2RC2'
+        traintrialtypes = [111 110];
+        probetrialtypes = [1109];
+    otherwise
+        error(['set ' svmdesc])
+end
+
+Ntt = numel(traintrialtypes);
+Nprobett = numel(probetrialtypes);
+Nalltt = numel(alltrialtypes);
+
+SVMout.Nneurons = Nneurons;
+% SVMout.exptid = ICblocks{b};
+% SVMout.ICtrialtypes = ICtrialtypes;
+SVMout.trialtypes = traintrialtypes;
+
+SVMout.numtrials = zeros(Ntt,1);
+% DI.numtrialpairs = zeros(Ntt,Ntt);
+for typi1 = 1:Ntt
+    SVMout.numtrials(typi1) = nnz(trialorder==SVMout.trialtypes(typi1));
+end
+
+% balance trials
+cftttrials = ismember(trialorder, SVMout.trialtypes);
+Ntrialspertype = min(SVMout.numtrials);
+if all(SVMout.numtrials==Ntrialspertype)
+    trials2anal = cftttrials;
+    SVMout.analtrials = find(trials2anal);
+else
+    warning('balancing number of trials')
+    trials2anal = false(numrectrials,1);
+    for typi1 = 1:Ntt
+        trialsintype = find(trialorder==SVMout.trialtypes(typi1));
+        trialsintype = trialsintype(1:Ntrialspertype);
+        trials2anal(trialsintype) = true;
+    end
+    if all(cftttrials(trials2anal)) && ~any(trials2anal(~cftttrials))
+        SVMout.analtrials = find(trials2anal);
+    else
+        error('trials to analyze was not selected correctly')
+    end
+end
+SVMout.analtriallabels = trialorder(trials2anal);
+
+Ntesttrialspertype = floor(Ntrialspertype/Nsplits);
+Ntraintrialspertype = Ntrialspertype - Ntesttrialspertype;
+SVMout.Ntt = Ntt;
+SVMout.Ntrialspertype = Ntrialspertype;
+SVMout.Ntraintrialspertype = Ntraintrialspertype;
+
+Ntraintrials = Ntt*Ntraintrialspertype;
+Ntesttrials = Ntt*(Ntrialspertype-Ntraintrialspertype);
+
+% probe trials
+probetrials = ismember(trialorder, probetrialtypes);
+SVMout.probetrials = find(probetrials);
+
+alltrials = true(size(trialorder));
+SVMout.alltrials = find(alltrials);
+
+SVMout.trialorder = trialorder;
+
+% randtrialorder=randperm(numrectrials);
+% SVMout.randtrialorder = randtrialorder;
+% trials2anal = randtrialorder(ismember(randtrialorder, SVMout.analtrials));
+
+SVM_models.(whichR) = cell(1, Nsplits);
+
+SVMout.(whichR).traintrialinds = zeros(Ntraintrials, Nsplits);
+SVMout.(whichR).testtrialinds = zeros(Ntesttrials, Nsplits);
+
+% SVMout.(whichR).Ylabs = cell(Ntt, Nsplits);
+
+% Nsplits-fold cross-validation
+trials2analind = find(trials2anal); % consider randomizing the order of this
+C = cvpartition(trialorder(trials2analind),'KFold',Nsplits, 'Stratify',true);
+if ~( all(C.TrainSize==Ntraintrials) && all(C.TestSize==Ntesttrials) )
+    error('check balancing trials')
+end
+for isplit = 1:Nsplits
+    close all
+    ttclk = tic;
+    
+    %{
+                testtrialinds = zeros(Ntesttrials,1);
+                traintrialinds = zeros(Ntraintrials,1);
+                for typi1 = 1:Ntt
+                    tempinds = trials2anal( trialorder(trials2anal)==SVMout.trialtypes(typi1) );
+                    tempinds = reshape(tempinds,[],1);
+                    if size(tempinds,1) ~= Ntrialspertype
+                        error('Ntrialspertype not consistent between trial types? check')
+                    end
+                    temptestintype = false(Ntrialspertype,1);
+                    temptestintype((isplit-1)*Ntesttrialspertype+1:isplit*Ntesttrialspertype) = true;
+                    temptrainintype = true(Ntrialspertype,1);
+                    temptrainintype((isplit-1)*Ntesttrialspertype+1:isplit*Ntesttrialspertype) = false;
+                    testtrialinds((typi1-1)*Ntesttrialspertype+1:typi1*Ntesttrialspertype) = tempinds(temptestintype);
+                    traintrialinds((typi1-1)*Ntraintrialspertype+1:typi1*Ntraintrialspertype) = tempinds(temptrainintype);
+                end
+                testtrialinds = trials2anal(ismember(trials2anal, testtrialinds));
+                traintrialinds = trials2anal(ismember(trials2anal, traintrialinds));
+    %}
+    
+    idxTrain = training(C,isplit);
+    traintrialinds = trials2analind(idxTrain);
+    idxTest = test(C,isplit);
+    testtrialinds = trials2analind(idxTest);
+    
+    if any(ismember(traintrialinds, testtrialinds))
+        error('train and test trials should not overlap')
+    end
+    if ~( all(ismember(trialorder(testtrialinds), SVMout.trialtypes)) && all(ismember(trialorder(traintrialinds), SVMout.trialtypes)) )
+        error('train and test trials of incorrect type detected')
+    end
+    
+    SVMout.(whichR).traintrialinds(:,isplit) = traintrialinds;
+    SVMout.(whichR).testtrialinds(:,isplit) = testtrialinds;
+    
+    switch preproc
+        case 'none'
+            Tp = tempR';
+        case 'zscore'
+            % Z-score
+            trainRmean = mean(tempR(:,traintrialinds),2);
+            trainRstd = std(tempR(:,traintrialinds),0,2);
+            
+            Tp = ( (tempR-trainRmean)./trainRstd )';
+            Tp(isnan(Tp))=0;
+        case 'minmax'
+            trainRmin = min(tempR(:,traintrialinds),[],2);
+            trainRrange = range(tempR(:,traintrialinds),2);
+            
+            Tp = ( (tempR-trainRmin)./trainRrange )';
+        case 'meancenter'
+            trainRmean = mean(tempR(:,traintrialinds),2);
+            
+            Tp = (tempR-trainRmean)';
+    end
+    
+    X = Tp(traintrialinds,:);
+    Y = reshape(trialorder(traintrialinds),[],1);
+    
+    %                 X = X(randomizedtraintrialorder, :);
+    %                 Y = Y(randomizedtraintrialorder);
+    
+    Xtest = Tp(testtrialinds,:);
+    Ytest = reshape(trialorder(testtrialinds),[],1);
+    
+    Xprobe = Tp(probetrials,:);
+    Xall = Tp(alltrials,:);
+    
+    % t is an SVM template. Most of its properties are empty.
+    % When the software trains the ECOC classifier, it sets the applicable properties to their default values.
+    % Train the ECOC classifier using the SVM template.
+    % Transform classification scores to class posterior probabilities
+    % (which are returned by predict or resubPredict) using the 'FitPosterior' name-value pair argument.
+    % Specify the class order using the 'ClassNames' name-value pair argument.
+    % Display diagnostic messages during training by using the 'Verbose' name-value pair argument.
+    
+    Ylabs = unique(Y);
+    SVMout.(whichR).Ylabs = Ylabs;
+    
+    switch whichSVMkernel
+        case 'RBF'
+            t = templateSVM('Standardize',true,'KernelFunction', 'rbf');
+        case 'Linear'
+            t = templateSVM('Standardize',true,'KernelFunction', 'linear');
+        case 'Poly2'
+            t = templateSVM('Standardize',true,'KernelFunction', 'polynomial' , 'PolynomialOrder', 2);
+    end
+    if optimizeSVM
+        SVMModel = fitcecoc(X,Y,'Learners',t,'FitPosterior',false, ...
+            'ClassNames', Ylabs, 'Verbose',0, 'OptimizeHyperparameters', 'auto', ...
+            'HyperparameterOptimizationOptions', struct('UseParallel',true, 'ShowPlots', false));
+    else
+        SVMModel = fitcecoc(X,Y,'Learners',t,'FitPosterior',false, 'ClassNames', Ylabs, 'Verbose',0);
+    end
+    %                 CVMdl = crossval(SVMModel);
+    
+    
+    SVM_models.(whichR){isplit} = SVMModel;
+    
+    for t = 1:4
+        switch t
+            case 1
+                Xtemp = X;
+                Ytemp = Y;
+                tempSVMmodel = SVMModel;
+                svmmd = 'train';
+            case 2
+                Xtemp = Xtest;
+                Ytemp = Ytest;
+                tempSVMmodel = SVMModel;
+                svmmd = 'test';
+            case 3
+                Xtemp = Xprobe;
+                tempSVMmodel = SVMModel;
+                svmmd = 'probe';
+            case 4
+                Xtemp = Xall;
+                tempSVMmodel = SVMModel;
+                svmmd = 'all';
+        end
+        [templabel,tempscore] = predict(tempSVMmodel,Xtemp);
+        SVMout.(whichR).(svmmd).label(:,isplit) = templabel;
+        SVMout.(whichR).(svmmd).score(:,:,isplit) = tempscore;
+    end
+    
+    
+    fprintf('%s %s %s %s %d/%d\n', mousedate, whichSVMkernel, whichvisarea, whichICblock, isplit, Nsplits)
+    toc(ttclk)
+end
+
+
+fprintf('%s %s %s %s done!\n', mousedate, whichSVMkernel, whichvisarea, whichICblock)
+toc(sesclk)
